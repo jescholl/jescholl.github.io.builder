e1fc7bb566dc1e9bee58ecf19b0729576e7455c8
Jason Scholl <jason.e.scholl@gmail.com>
2017-02-04 09:49:24 -0800

    move to .builder repo

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 set -e
 
+if [ -z "$TARGET_REPO_URL" ]; then
+  # if TARGET_REPO_URL isn't set, attempt to remove ".builder" from this repo's url
+  TARGET_REPO_URL=${CIRCLE_REPOSITORY_URL%%.builder}
+fi
+
+if [ -z "$TARGET_REPO_BRANCH" ]; then
+  TARGET_REPO_BRANCH=master
+fi
+
 #copy compiled site to artifacts
 rsync -a _site/ $CIRCLE_ARTIFACTS
 
-#reset repo and checkout gh-pages
-git fetch origin && \
-git clean -fd && \
-git checkout master && \
-git reset --hard master && \
-git checkout gh-pages && \
-git reset --hard gh-pages
-git clean -fd
+
+#clone target repo to a temp dir to ensure no name collisions
+TMPDIR=$(mktemp -d)
+git clone $TARGET_REPO_URL $TMPDIR/target_repo
+cd $TMPDIR/target_repo
+
+#verify clean repo
+git fetch origin
+git checkout $TARGET_REPO_BRANCH
 
 #configure git commiter
 git config --global user.name "CI"
@@ -24,7 +34,7 @@ rsync -a $CIRCLE_ARTIFACTS/ .
 git add .
 git commit -m "Updated by CI - All tests passed
 $CIRCLE_BUILD_URL"
-git push origin gh-pages
+git push origin master
 
 #ping search engines with new sitemap
-curl "http://www.google.com/webmasters/sitemaps/ping?sitemap=http://jescholl.com/sitemap.xml"
+curl "http://www.google.com/webmasters/sitemaps/ping?sitemap=https://jescholl.com/sitemap.xml"
