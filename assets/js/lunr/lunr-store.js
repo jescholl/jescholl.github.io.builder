a124d922856ad61c20d8ae0bbba931bf2828c3d2
CI <ci@jescholl.com>
2018-04-12 12:14:53 -0700

    Add post on custom Jekyll plugins

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "Short Selling Houses",
+        "excerpt":"Since the housing market in Portland (and my neighborhood especially) is reallyhot right now I’ve been thinking a lot about home values and whether or not to sell.One bit of advice I heard a few years ago that I’ve always been intrigued by was theidea of selling your home in...","categories": ["finance"],
+        "tags": [],
+        "url": "https://jescholl.com/finance/short-selling-houses/",
+        "teaser":"https://jescholl.com/images/portland_home_values.png"},{
+        "title": "I'm in ops",
+        "excerpt":"I just got back from Monitorama and was once againreminded how terrible job titles are in the tech field. At each of the socialgatherings I would set off to meet new people, and by far the most commonintroduction was “Hello my name is …, and I’m in ops”. I did...","categories": ["general"],
+        "tags": [],
+        "url": "https://jescholl.com/general/im-in-ops/",
+        "teaser":"https://jescholl.com/images/monitorama-2016.jpg"},{
+        "title": "Custom Jekyll plugins on github-pages",
+        "excerpt":"Github-pages is fantastic, they provide free hosting for static sites, or Jekyllsites that stick to the list of whitelisted gems.Traditional wisdom is that if you want to use Jekyll with plugins Github doesn’t allow,you need to host it on Amazon’s S3, or some other platform that isn’t as restrictive as...","categories": ["automation"],
+        "tags": [],
+        "url": "https://jescholl.com/automation/using-custom-jekyll-plugins-on-github/",
+        "teaser":"https://jescholl.com/images/html.jpg"}]
