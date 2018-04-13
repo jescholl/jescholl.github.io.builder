18d211e541d924315abe1acc229783866e238b15
CI <ci@jescholl.com>
2018-04-13 10:28:06 -0700

    improve contact info on resume

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,15 +2,15 @@ var store = [{
         "title": "Short Selling Houses",
         "excerpt":"Since the housing market in Portland (and my neighborhood especially) is reallyhot right now I’ve been thinking a lot about home values and whether or not to sell.One bit of advice I heard a few years ago that I’ve always been intrigued by was theidea of selling your home in...","categories": ["finance"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/finance/short-selling-houses/",
-        "teaser":"http://0.0.0.0:4000/images/portland_home_values.png"},{
+        "url": "https://jescholl.com/finance/short-selling-houses/",
+        "teaser":"https://jescholl.com/images/portland_home_values.png"},{
         "title": "I'm in ops",
         "excerpt":"I just got back from Monitorama and was once againreminded how terrible job titles are in the tech field. At each of the socialgatherings I would set off to meet new people, and by far the most commonintroduction was “Hello my name is …, and I’m in ops”. I did...","categories": ["general"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/general/im-in-ops/",
-        "teaser":"http://0.0.0.0:4000/images/monitorama-2016.jpg"},{
+        "url": "https://jescholl.com/general/im-in-ops/",
+        "teaser":"https://jescholl.com/images/monitorama-2016.jpg"},{
         "title": "Custom Jekyll plugins on github-pages",
         "excerpt":"Github-pages is fantastic, they provide free hosting for static sites, or Jekyllsites that stick to the list of whitelisted gems.Traditional wisdom is that if you want to use Jekyll with plugins Github doesn’t allow,you need to host it on Amazon’s S3, or some other platform that isn’t as restrictive as...","categories": ["automation"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/automation/using-custom-jekyll-plugins-on-github/",
-        "teaser":"http://0.0.0.0:4000/images/html.jpg"}]
+        "url": "https://jescholl.com/automation/using-custom-jekyll-plugins-on-github/",
+        "teaser":"https://jescholl.com/images/html.jpg"}]
